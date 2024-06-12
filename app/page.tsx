4c4260fd5c4ee7dcc4f4e6d5df3ed37fd0f9efc4
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-06-12 22:40:12 +0300

    Renamed class

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import GetStartedModule from "@/app/modules/getStarted/GetStartedModule";
 export default function Home() {
     return (
         <>
-            <div className="centralized-block">
+            <div className="block--centralized">
                 <div className='container'>
                     <h1 style={{textAlign: "center"}}>Build Trust, Gain Devoted Customers</h1>
                     <p style={{textAlign: "center", width: "60%"}}>Build Your Audience & Get Devoted Customers with Agile Alpaca, Marketing Agency Targeted at Human Relationships</p>
