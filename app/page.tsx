7f07295321ca34f4255b26dc189f3145b861addf
Reißieck <98209882+Wlad1slav@users.noreply.github.com>
2024-06-13 19:00:55 +0300

    Added information cards to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
+import React from "react";
+
 import GetStartedModule from "@/app/modules/getStarted/GetStartedModule";
+import InformationCard from "@/app/components/informationCard/InformationCard";
+import Icon from "@/app/components/icon/Icon";
+
 
 export default function Home() {
     return (
@@ -11,6 +16,56 @@ export default function Home() {
 
                 <GetStartedModule />
             </div>
+
+            <div className={'block--bright'}>
+                <InformationCard
+                    title='Web Development'
+                    subtitle='A site for your business'
+                    hasBack={true}
+                    mainIcon={<Icon.web fill="fill--standard" className="icon--main" />}
+                    mainIconPlace={'left'}
+                    position={{
+                        left: "15%",
+                        top: "10%"
+                    }}
+                    additionalIcons={[
+                        <Icon.wordpress fill='fill--wordpress' />,
+                        <Icon.react fill='fill--react' />,
+                        <Icon.vue fill='fill--vue' />,
+                        <Icon.nodejs fill='fill--nodejs' />,
+                        <Icon.sass fill='fill--sass' />,
+                        <Icon.laravel fill='fill--laravel' />,
+                    ]}
+                >
+                    Let’s bring that dream to life! We craft beautiful websites with love, backed by thorough UX research and tailored designs.
+                </InformationCard>
+
+                <InformationCard
+                    title='Inbound Marketing'
+                    mainIcon={<Icon.growUp fill="fill--standard" className="icon--main" />}
+                    mainIconPlace={'near-title'}
+                    position={{
+                        left: "60%",
+                        top: "30%"
+                    }}
+                >
+                    Feeling like you’re not catching enough leads? Don’t worry! We’re here to sprinkle some magic with
+                    strategic inbound marketing, account-based marketing, and digital advertising.
+                </InformationCard>
+
+                <InformationCard
+                    title='CRM Integration'
+                    mainIcon={<Icon.hubspot fill="fill--standard" className="icon--main" />}
+                    mainIconPlace={'top'}
+                    position={{
+                        left: "25%",
+                        top: "65%"
+                    }}
+                    width='200px'
+                />
+            </div>
+
+
         </>
     );
 }
